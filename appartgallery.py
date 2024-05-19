145a4d0d6fdf4af31ce41a4a36f7333fcb4b76e3
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-20 11:30:14 +1200

    added many options with a while True loop

--- appartgallery.py
+++ appartgallery.py
@@ -1,4 +1,4 @@
-# docstring - Elissa Piao - Artgallery datbase project
+# docstring - Elissa Piao - Artgallery database project
 
 #import
 
@@ -9,3 +9,15 @@ import sqlite3
 #Constants and variables
 
 DATABASE = "artgallery.db"
+
+
+
+#functions
+
+#def
+
+
+#main code
+
+while True:
+    userinput = input("\nWelcome to the Art Gallery database!\nHere are some options.\n\n1.View all artworks with artist information.\n2.View all artworks sorted alphabetically\n3.View all artworks sorted by year\n4.View all artworks sorted by price\n10.View all artists.\n11.View all artists sorted by country.\n12.View all artists sorted by d.o.b.")

1c7881cb94ae02d69a44e3797b1d603bb79042fb
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-22 11:52:10 +1200

    Fixed artworks sorted alphabetically

--- appartgallery.py
+++ appartgallery.py
@@ -1,13 +1,9 @@
 # docstring - Elissa Piao - Artgallery database project
-
 #import
 import sqlite3
 
-
 #Constants and variables
 DATABASE = "artgallery.db"
-
-
 
 #functions
 
@@ -35,14 +31,37 @@ def print_all_artworks_with_artists():
             
     db.close()
 
-
 def print_all_artworks_sorted_alphabetically():
     '''Print all artworks with artists sorted alphabetically(A-Z)'''
     db = sqlite3.connect(DATABASE)
     
     cursor = db.cursor()
 
-    sql = "SELECT Artwork.Name, Artwork.style, Artwork.Year_made, Artwork.Price, Artist.Artist_name, Artist.Date_of_birth, Artist.Country FROM Artwork JOIN Artist ON Artist.id=Artwork.Artist ORDER BY Artwork.Name ASC;"
+    sql = "SELECT Artwork.Name, Artist.Artist_name, Artwork.style, Artwork.Year_made, Artwork.Price FROM Artwork JOIN Artist ON Artist.id=Artwork.Artist ORDER BY Artwork.Name ASC;"
+
+    cursor.execute(sql)
+
+    results = cursor.fetchall()
+
+    #loop through results
+
+    print(f"                       Name             Artist               Style    Year made    Price($)\n")
+
+    for Artwork in results:
+            
+            print(f"{Artwork[0]:>27}{Artwork[1]:>20}{Artwork[2]:>20}{Artwork[3]:>13}{Artwork[4]:>10}M")
+
+    #loop finishes here
+        
+    db.close()
+
+def print_all_artworks_sorted_by_year_asc():
+    '''Print all artworks with artists sorted year ascending'''
+    db = sqlite3.connect(DATABASE)
+    
+    cursor = db.cursor()
+
+    sql = "SELECT Artwork.Name, Artwork.style, Artwork.Year_made, Artwork.Price, Artist.Artist_name, Artist.Date_of_birth, Artist.Country FROM Artwork JOIN Artist ON Artist.id=Artwork.Artist ORDER BY Artwork.Year_made ASC;"
 
     cursor.execute(sql)
 
@@ -60,10 +79,60 @@ def print_all_artworks_sorted_alphabetically():
         
     db.close()
 
-#main code
+def print_all_artworks_sorted_by_price_asc():
+    '''Print all artworks with artists sorted year ascending'''
+    db = sqlite3.connect(DATABASE)
+    
+    cursor = db.cursor()
 
+    sql = "SELECT Artwork.Name, Artwork.style, Artwork.Year_made, Artwork.Price, Artist.Artist_name, Artist.Date_of_birth, Artist.Country FROM Artwork JOIN Artist ON Artist.id=Artwork.Artist ORDER BY Artwork.Price ASC;"
+
+    cursor.execute(sql)
+
+    results = cursor.fetchall()
+
+    #loop through results
+
+    print(f"                       Name               Style    Year made    Price($)             Artist   D.O.B        Country\n")
+
+    for Artwork in results:
+            
+            print(f"{Artwork[0]:>27}{Artwork[1]:>20}{Artwork[2]:>13}{Artwork[3]:>10}M{Artwork[4]:>20}{Artwork[5]:>8}{Artwork[6]:>15}")
+
+    #loop finishes here
+        
+    db.close()
+
+    '''Print all artworks with artists sorted year ascending'''
+    db = sqlite3.connect(DATABASE)
+    
+    cursor = db.cursor()
+
+    sql = "SELECT Artwork.Name, Artwork.style, Artwork.Year_made, Artwork.Price, Artist.Artist_name, Artist.Date_of_birth, Artist.Country FROM Artwork JOIN Artist ON Artist.id=Artwork.Artist ORDER BY Artwork.Year_made ASC;"
+
+    cursor.execute(sql)
+
+    results = cursor.fetchall()
+
+    #loop through results
+
+    print(f"                       Name               Style    Year made    Price($)             Artist   D.O.B        Country\n")
+
+    for Artwork in results:
+            
+            print(f"{Artwork[0]:>27}{Artwork[1]:>20}{Artwork[2]:>13}{Artwork[3]:>10}M{Artwork[4]:>20}{Artwork[5]:>8}{Artwork[6]:>15}")
+
+    #loop finishes here
+        
+    db.close()
+
+
+#main code
+#Welcome user
+print("Welcome to the Art Gallery database!\n")
+#username = input("What is your name?")
 while True:
-    userinput = input("\nWelcome to the Art Gallery database!\nHere are some options.\n\n1.View all artworks with artist information.\n2.View all artworks sorted alphabetically\n3.View all artworks sorted by year\n4.View all artworks sorted by price\n10.View all artists.\n11.View all artists sorted by country.\n12.View all artists sorted by d.o.b.\n")
+    userinput = input("\nHere are some options.\n\n1.View all artworks with artist information.\n2.View all artworks sorted alphabetically\n3.View all artworks sorted by year\n4.View all artworks sorted by price\n10.View all artists.\n11.View all artists sorted by country.\n12.View all artists sorted by d.o.b.\n")
     if userinput == "1":
         print_all_artworks_with_artists()
     elif userinput == "2":

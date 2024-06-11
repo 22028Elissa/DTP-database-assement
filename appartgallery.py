3f6e5a4d1a079acb1c849449a9dc46c563c8275d
22028Elissa <22028@burnside.school.nz>
2024-06-11 20:33:40 +1200

    created import smtp library

--- appartgallery.py
+++ appartgallery.py
@@ -6,6 +6,11 @@ import sqlite3
 DATABASE = "artgallery.db"
 
 #functions
+#import smtp library
+import os
+from email.message import EmailMessage
+import ssl
+import smtplib
 
 #login/create acc
 
@@ -93,21 +98,16 @@ def print_all_artworks_sorted_with_where(specific):
 
     results = cursor.fetchall()
 
-    #if there are results
-    if results:
-    
-        #loop through results
+    #loop through results
 
-        print(f"                       Name             Artist               Style     Year made    Price($)\n")
+    print(f"                       Name             Artist               Style     Year made    Price($)\n")
 
-        for Artwork in results:
-                
-                print(f"{Artwork[0]:>27}{Artwork[1]:>20}{Artwork[2]:>20}{Artwork[3]:>13}{Artwork[4]:>10}M")
+    for Artwork in results:
+            
+            print(f"{Artwork[0]:>27}{Artwork[1]:>20}{Artwork[2]:>20}{Artwork[3]:>13}{Artwork[4]:>10}M")
 
-        #loop finishes here
-    else:
-        print(f"\nThere {printcolumn} {specific}.")
-
+    #loop finishes here
+        
     db.close()
  
 def print_all_artists_sorted_with_where(specific):
@@ -122,19 +122,15 @@ def print_all_artists_sorted_with_where(specific):
 
     results = cursor.fetchall()
 
-    
-    #if there are results
-    if results:
-         
-        print(f"                  Name   Year born        Country\n")
-        #loop through results
-        for Artist in results:
+    #loop through results
+
+    print(f"                  Name   Year born        Country\n")
+
+    for Artist in results:
             
-                print(f"{Artist[0]:>22}{Artist[1]:>12}{Artist[2]:>15}")
-                #loop finishes here
-    else:
-        print(f"\nThere {printcolumn} {specific}.")
-            
+            print(f"{Artist[0]:>22}{Artist[1]:>12}{Artist[2]:>15}")
+
+    #loop finishes here
         
     db.close()
 
@@ -150,18 +146,15 @@ def print_all_artworks_sorted_with_where_with_between(x,y):
 
     results = cursor.fetchall()
 
-    
-    #if there are results
-    if results:
-        print(f"                       Name             Artist               Style     Year made    Price($)\n")
-         #loop through results
-        for Artwork in results:
+    #loop through results
+
+    print(f"                       Name             Artist               Style     Year made    Price($)\n")
+
+    for Artwork in results:
             
-                print(f"{Artwork[0]:>27}{Artwork[1]:>20}{Artwork[2]:>20}{Artwork[3]:>13}{Artwork[4]:>10}M")
-                #loop finishes here
-    else:
-        print(f"\nThere were no artworks made from the years {x} and {y}.")
-    
+            print(f"{Artwork[0]:>27}{Artwork[1]:>20}{Artwork[2]:>20}{Artwork[3]:>13}{Artwork[4]:>10}M")
+
+    #loop finishes here
         
     db.close()
 
@@ -177,18 +170,15 @@ def print_all_artists_sorted_with_where_with_between(x,y):
 
     results = cursor.fetchall()
 
-    if results:
-        #loop through results
+    #loop through results
 
-        print(f"                  Name   Year born        Country\n")
+    print(f"                  Name   Year born        Country\n")
 
-        for Artist in results:
-                
-                print(f"{Artist[0]:>22}{Artist[1]:>12}{Artist[2]:>15}")
-                #loop finishes here
+    for Artist in results:
+            
+            print(f"{Artist[0]:>22}{Artist[1]:>12}{Artist[2]:>15}")
 
-    else:
-        print(f"There were no artist born in the years {x} and {y}.")
+    #loop finishes here
         
     db.close()
 
@@ -234,22 +224,20 @@ while True:
         print_all_artists_sorted_(sortedway)
     elif userinput == "8":   
         #There are more options including user searches.
-        while True: 
-            eightinput = input("1.Specifics on Artworks.\n2.Specifics on Artists.\n3.Return\n")
+        eightinput = input("1.Specifics on Artworks.\n2.Specifics on Artists.\n3.Return\n")
+        while True:    
             if eightinput == "1":
                 #These are options relating to artworks.
                 while True:
                     artvsartistinput = input("\n1.To find one 'artwork'.\n2.All artworks with one style\n3.All artworks where the year made falls under(x-y)\n4.Return\n")
                     if artvsartistinput == "1":
                         #this options finds a specific artwork the user searches for.
-                        printcolumn = "was no artwork called"
                         column = "Artwork.Name"
                         specific = input("Please type the name of the artwork you would like.(correctly with capitals): ")
                         print_all_artworks_sorted_with_where(specific)
                     elif artvsartistinput == "2":
                         #This option finds all artworks with a specific style the user would like.
                         column = "Artwork.Style"
-                        printcolumn = "were no artworks in the style,"
                         specific = input("Please type the style of the artworks you would like.(correctly with capitals): ")
                         print_all_artworks_sorted_with_where(specific)
                     elif artvsartistinput == "3":
@@ -271,24 +259,21 @@ while True:
                         print("That was not a valid option.") 
             elif eightinput == "2":
                 while True:
-                    #These are options relating to artists.
+                    #These are options relating to artists. 
                     artistvsart = input("\n1.To find one artist.\n2.Find all Artists from one Country.\n3.Find all Artworks by one artist.\n4.Find all artists born in specific years.\n5.Return\n")
                     if artistvsart == "1":
                         #This brings up specific information about only one artist
                         column = "Artist.Artist_name"
-                        printcolumn = "is no Artist called"
                         specific = input("Please type the name of the artist you would like(correctly with capitals): ")
                         print_all_artists_sorted_with_where(specific)
                     elif artistvsart == "2":
                         #This find all artists from a country the user chooses.
                         column = "Artist.Country"
-                        printcolumn = "were no artworks from"
                         specific = input("Please type the name of the country you would like(correctly with capitals): ")
                         print_all_artists_sorted_with_where(specific)
                     elif artistvsart == "3":
                         #This finds all paintings from one specific artist.
                         column = "Artist.Artist_name"
-                        printcolumn = "was no Artist called"
                         specific = input("Please type the name of the artist for the artworks you would like(correctly with capitals): ")
                         print_all_artworks_sorted_with_where(specific)
                     elif artistvsart == "4":
@@ -305,6 +290,8 @@ while True:
                                     print("Please enter numbers seperated by a comma.")    
                     elif artistvsart == "5":
                         break
+                else:
+                    print("That was not a valid option.") 
             elif eightinput == "3":
                 break
             else:
